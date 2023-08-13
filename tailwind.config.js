e2707fdb1a276d55a97062b12cd5aa10a59e28c2
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2023-08-13 09:49:23 +0900

    :sparkles: ポートフォリオサイトの一通りのテンプレートの作成

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        body: '#050505',
+        'selected-text': '#A3A3FF',
+        theme: '#5c318c',
+        secondary: '#9191A4',
+        badge: '#3F3F51',
+        inputBorder: '#565666',
+        input: '#2A2A33',
+      },
+    },
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+    },
   },
   plugins: [],
 }
